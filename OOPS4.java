80579461b9f3af05c13d12339373cc8be5aeeb9f
ashokbezawada <45648866+ashokbezawada@users.noreply.github.com>
2018-12-06 10:40:49 +0530

    java

---
+++ OOPS4.java
@@ -0,0 +1,53 @@
+Class Development
+{
+  private int a,b;
+  private char x;
+ public development()
+ {
+   a=10;
+ }
+
+ public Development(inta,intb)
+ {
+   this.a=a;
+   this.b=b;
+ }
+
+ public Development(charx,intb)
+ {
+   this.x=x;
+   this.b=b;
+ }
+ 
+ public void task1()
+ 
+ {
+  system.out.println("a value is :"+a);
+ }
+
+ public void task2()
+  
+  {
+   
+   system.out.println("a value is :"+a);
+   system.out.println("b value is :"+b);
+  }
+
+ public void task3()
+  
+   {
+     
+    system.out.println("x value is :"+x);
+    system.out.println("b value is :"+b);
+   }
+
+Class OOPS4
+
+  public class void main(string[]args)
+   {
+     Development d = new Development();
+     d.task1(x,5);
+     Development d2 = new Development(11,22);
+     d.task2();
+   }
+}
